24b7a331b218d25efb2a76d7f78f7ad8b9d1880c
James <james@jmew.ca>
2021-07-02 15:40:39 -0400

    (feat): add certifications page

---
+++ src/Data/Certifications.js
@@ -0,0 +1,29 @@
+const production = [
+	{
+		id: 'chyron-hego',
+		displayTitle: 'ChyronHego',
+		title: '<a rel="noopener noreferrer" href="https://chyronhego.com/" target="_blank">ChyronHego</a>',
+		link: './certifications/prime_badges.pdf',
+		badges: [
+			{
+				title: 'PRIME Yellow Belt',
+				image: './certifications/chyron/Certification_38358.png',
+			},
+			{
+				title: 'CHYRONHEGO White Belt',
+				image: './certifications/chyron/Certification_38422.png',
+			},
+		],
+	},
+];
+
+const coding = [];
+
+const certifications = {
+	production,
+	coding,
+};
+
+export { production, coding };
+
+export default certifications;
